0d212d086d6450095a965a65fc67d2ad9124ab7e
Jochen Klar <mail@jochenklar.de>
2020-08-26 14:42:41 +0200

    Refactor definitions

--- build/definitions.py
+++ build/definitions.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 def main():
     directory = Path('definitions')
-    definition_files = os.listdir(directory)
     definitions = {}
     for file_name in os.listdir(directory):
         file_path = directory / file_name
@@ -20,23 +19,23 @@ def main():
     for simulation_round in simulation_rounds:
         for product in products:
             for sector in sectors:
-                output_path = Path('output') / 'definitions' / simulation_round / product / sector
-                output_path.mkdir(parents=True, exist_ok=True)
+                output_path = Path('output').joinpath('definitions') \
+                                            .joinpath(simulation_round).joinpath(product).joinpath(sector) \
+                                            .with_suffix('.json')
+                output_path.parent.mkdir(parents=True, exist_ok=True)
 
-                with open(output_path / 'index.json', 'w') as f:
-                    f.write(json.dumps(definition_files, indent=2))
-
+                output_definitions = {}
                 for definition_name, rows in definitions.items():
                     # filter definitions for simulation_round, product, sector
-                    definition = []
+                    output_definitions[definition_name] = []
                     for row in rows:
                         if 'simulation_rounds' not in row or simulation_round in row['simulation_rounds']:
                             if 'products' not in row or product in row['products']:
                                 if 'sectors' not in row or sector in row['sectors']:
-                                    definition.append(row)
+                                    output_definitions[definition_name].append(row)
 
-                    with open(output_path.joinpath(definition_name).with_suffix('.json'), 'w') as f:
-                        f.write(json.dumps(definition, indent=2))
+                with open(output_path, 'w') as f:
+                    f.write(json.dumps(output_definitions, indent=2))
 
 
 if __name__ == "__main__":
